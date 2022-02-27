617183ae7e5f0fbc14d62d13a2f68bcffa538592
priyal <vyaspriyal302000@gmail.com>
2022-02-27 18:59:24 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require("express");
 const app = express()
 const port = 8000;
+const fs = require("fs");
+const mongoose = require("mongoose");
+
+
+mongoose.connect('mongodb://localhost:27017/priyal',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+var collection = db.collection("priyaldata");
 
 app.get('/',(req,res)=>{
    res.send("hello world")
 })
+
+app.get('/movies',(req,res)=>{
+   fs.readFile(__dirname + '/' + 'movie.json', 'utf8', (err, data) => {
+    res.end(data);
+   });
+})
+
+app.get("/personnel", (request, response) => {
+  collection.find({}).toArray((error, result) => {
+      if(error) {
+          return response.status(500).send(error);
+      }
+      response.send(result);
+  });
+});
 
 app.listen(port,()=>{
   console.log(`app listening at http://localhost:${port}`)
